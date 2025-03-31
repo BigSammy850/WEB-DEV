b87211ce41ecad2df13bc38fa36f1edf80ebe431
SAMUEL <samuelanyiam86@gmail.com>
2025-03-31 16:36:38 +0100

    add collision detection for player movement; update wall and enemy elements with 'avoid' class

--- WEEK 10/ex1/script.js
+++ WEEK 10/ex1/script.js
@@ -33,23 +33,47 @@ function move() {
     const position = player.getBoundingClientRect();
 
     if (downPressed == true) {
-        player.style.top = position.top + 1 + 'px';
+        let newBottom = position.bottom + 1;
+
+        let btmL = document.elementFromPoint(position.left, newBottom);
+        let btmR = document.elementFromPoint(position.right, newBottom);
+        if (btmL.classList.contains('avoid') == false && btmR.classList.contains('avoid') == false) {
+            player.style.top = position.top + 1 + 'px';
+        }
     }
 
     if (leftPressed == true) {
-        player.style.left = position.left - 1 + 'px';
+        let newLeft = position.left - 1;
+
+        let leftT = document.elementFromPoint(newLeft, position.top);
+        let leftB = document.elementFromPoint(newLeft, position.bottom);
+        if (leftT.classList.contains('avoid') == false && leftB.classList.contains('avoid') == false) {
+            player.style.left = position.left - 1 + 'px';
+        }
     }
 
     if (upPressed == true) {
-        player.style.top = position.top - 1 + 'px';
+        let newTop = position.top - 1;
+
+        let topL = document.elementFromPoint(position.left, newTop);
+        let topR = document.elementFromPoint(position.right, newTop);
+        if (topL.classList.contains('avoid') == false && topR.classList.contains('avoid') == false) {
+            player.style.top = position.top - 1 + 'px';
+        }
     }
 
     if (rightPressed == true) {
-        player.style.left = position.left + 1 + 'px';
+        let newRight = position.right + 1;
+
+        let rightT = document.elementFromPoint(newRight, position.top);
+        let rightB = document.elementFromPoint(newRight, position.bottom);
+        if (rightT.classList.contains('avoid') == false && rightB.classList.contains('avoid') == false) {
+            player.style.left = position.left + 1 + 'px';
+        }
     }
 }
 
-setInterval(move, 5);
+setInterval(move, 0.5); 
 
 document.addEventListener('keyup', keyUp);
 document.addEventListener('keydown', keyDown);
