f4e08d7a6abee3afdf3f280030219b62b647a42e
SAMUEL <samuelanyiam86@gmail.com>
2025-04-28 15:44:04 +0100

    add dice rolling functionality; implement click event to display random results

---
+++ WEEK 11/ex1.js
@@ -0,0 +1,44 @@
+let dice = document.querySelectorAll(".side1");
+
+for (i = 0; i < dice.length; i++) {
+    //let text = dice[i].nextElementSibling;
+    //let textClick=  textpreviousSibling;
+    dice[i].addEventListener("click", function () {
+        let random = Math.ceil(Math.random() * 6);
+        this.classList = 'side' + random;
+        this.nextElementSibling.innerHTML = "You rolled a " + random;
+        //text.innerHTML = "You rolled a " + random;
+    });
+}
+
+// let random=mathfloor(Math.random()*6)+1;
+//     this.classList = 'side' + random;
+
+
+// let myArray = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// for (let i = 0; i < myArray.length; i++) {
+//     for (let j = 0; j < myArray[i].length; j++) {
+//         console.log(myArray[i][j]);
+//     }
+// }
+
+// for(let row of myArray){
+//     for(let column of row){
+//         console.log(column);
+//     }
+// }
+// let maze = [
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0]
+// ];
+
+// let row = Math.floor(Math.random() * maze.length);
+// let column = Math.floor(Math.random() * maze[row].length);
+// maze[row][column] = 1;
+// console.log(maze)
